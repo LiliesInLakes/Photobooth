f30e489e65dcf99b068537980aed8654c3210a71
Avni Agarwal <avniag27@gmail.com>
2026-06-03 16:38:32 +0530

    added imgs to filmstrip

--- main.py
+++ main.py
@@ -20,8 +20,8 @@ while True:
         t_lower = 40  
         t_upper = 90 
         canny = cv.Canny(gaus, t_lower, t_upper)
-        top, bottom, left, right = 20, 20, 20, 20
-        frame_color = [0, 255, 0]
+        top, bottom, left, right = 30, 30, 30, 30
+        frame_color = [0, 255, 255]
         # cv.imshow('Camera Feed', canny)
         cv.imshow('og', frame) # Display the frame
         Mask = cv.bitwise_not(canny)
@@ -29,12 +29,20 @@ while True:
         # out.write(fliped)
         #fourcc = cv2.VideoWriter_fourcc(*'XVID') # Example for .avi
         # Or for .mp4:
+        filmstrip= cv.imread("film.jpg")
         #fourcc = cv.VideoWriter_fourcc(*'MJPEG')
+        Mask= cv.cvtColor(Mask, cv.COLOR_GRAY2BGR)
+        canny= cv.cvtColor(canny, cv.COLOR_GRAY2BGR)
         frame1= cv.copyMakeBorder(Mask, top, bottom, left, right, borderType=cv.BORDER_CONSTANT, value=frame_color )
         frame2= cv.copyMakeBorder(canny, top, bottom, left, right, borderType=cv.BORDER_CONSTANT, value=frame_color)
         vertical= np.concatenate(( frame1, frame2), axis=0)
         cv.imshow('together', vertical)
         # Press 'q' to exit the loop
+        resized_img = cv.resize(Mask, (404, 300))
+        img2=cv.resize(canny, (404, 300))
+        filmstrip[82:82+ resized_img.shape[0], 165: 165+resized_img.shape[1]]= resized_img
+        filmstrip[416:416+ resized_img.shape[0], 165: 165+resized_img.shape[1]]= img2
+        cv.imshow("photobooth", filmstrip)
         if cv.waitKey(50) & 0xFF == ord('q'):
             break
 camera.release()
